70daa78f1f989fd633d3837370cb204225002064
wmlutz <william.meany.lutz@gmail.com>
2017-01-07 15:57:36 -0500

    added views for pages, next step express endpoints and data as well as nav links

--- client/src/directives/navBar.js
+++ client/src/directives/navBar.js
@@ -2,7 +2,7 @@ angular.module("obviapp").directive('obNavBar', function(){
   return {
     replace: true,
     restrict: "E",
-    templateUrl: "/../src/templates/navBar.html",
+    templateUrl: "/../src/templates/navbar/index.html",
     // controller: function($scope, $location){
     //   $scope.isPage = function(name){
     //     return new RegExp("/" + name + "($|/)").test($location.path());
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,3 +1,22 @@
 angular.module('obviapp').config(function($routeProvider){
-  $routeProvider;
+  $routeProvider
+  .when('/', {
+    redirectTo: '/login'
+  })
+  .when('/login', {
+    templateUrl: '/src/templates/login/index.html',
+    controller: 'LoginController'
+  })
+  .when('/leads', {
+    templateUrl: '/src/templates/leads/index.html',
+    controller: 'LeadUploadController'
+  })
+  .when('/interaction', {
+    templateUrl: '/src/templates/interaction/index.html',
+    controller: 'InteractionShowController'
+  })
+  .when('/review', {
+    templateUrl: '/src/templates/review/index.html',
+    controller: 'ReviewController'
+  });
 });
