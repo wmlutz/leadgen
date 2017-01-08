57175047c9e8ab3a45e114b1582ee524090f8fdd
wmlutz <william.meany.lutz@gmail.com>
2017-01-08 10:27:57 -0500

    set up for next page on login page, small update

--- client/src/controllers/loginController.js
+++ client/src/controllers/loginController.js
@@ -1,5 +1,8 @@
-angular.module('obviapp').controller('loginController', function() {
+angular.module('obviapp').controller('loginController', function($state) {
 	this.saveUser = function(user) {
     console.log("User: ", user);
+
+    // Should send to the next page upon save, haven't even started to look at if I'm doing thi right
+    // $state.go('/leads');
 	};
 });
